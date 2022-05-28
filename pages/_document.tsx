7ad729b427241f9ed2922645964b40ac425c663c
veremey <veremey4uk@gmail.com>
2022-05-28 12:49:53 +0300

    feat: custom document; component Htag;

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx:DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {...initialProps}
+  }
+
+  render():JSX.Element {
+    return (
+      <Html lang="ru">
+        <Head> 
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link 
+              href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;500;600&family=Noto+Sans:wght@300;400;500;600&display=swap"
+              rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
