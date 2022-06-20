3aa8f11d24501c3b415e343c64ea942f97d1b5a7
veremey <veremey4uk@gmail.com>
2022-06-20 23:32:43 +0300

    Feat: menu icons

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../context/app.context'
-
 import { MenuProps } from './Menu.props'
+
+import CoursesIcon from './icons/courses.svg'
+import ServicesIcon from './icons/services.svg'
+import BooksIcon from './icons/books.svg'
+import ProductsIcon from './icons/products.svg'
 
 const Menu = () => {
   const { menu, firstCategory, setMenu } = useContext(AppContext)
   return (
     <ul>
       {menu.map((m) => (
-        <li key={m._id}>{m._id.secondCategory}</li>
+        <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
       ))}
     </ul>
   )
