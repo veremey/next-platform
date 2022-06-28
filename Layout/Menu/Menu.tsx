fb6994844dae43b8af9c452222c3046a25ae0b26
veremey <veremey4uk@gmail.com>
2022-06-28 10:34:59 +0300

    feat: menu for multiple names + same functionality

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -1,25 +1,12 @@
 import React, { useContext } from 'react'
-import { AppContext } from '../../context/app.context'
-import { TopLevelCategory } from '../../interfaces/page.interface'
-import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface'
 import cn from 'classnames'
-import { MenuProps } from './Menu.props'
-
-import CoursesIcon from './icons/courses.svg'
-import ServicesIcon from './icons/services.svg'
-import BooksIcon from './icons/books.svg'
-import ProductsIcon from './icons/products.svg'
-
-import styles from './Menu.module.scss'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { AppContext } from '../../context/app.context'
+import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface'
 
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-  { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-  { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-  { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-]
+import styles from './Menu.module.scss'
+import { firstLevelMenu } from '../../helpers/helpers'
 
 const Menu = () => {
   const { menu, firstCategory, setMenu } = useContext(AppContext)
