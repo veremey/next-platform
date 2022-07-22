4e3f5ba2704547ffe71560309c5860c842bdf96a
veremey <veremey4uk@gmail.com>
2022-07-22 23:14:27 +0300

    feat: search

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -5,14 +5,24 @@ import cn from 'classnames'
 import styles from './Search.module.scss'
 import GlassIcon from './images/glass.svg'
 import { Button } from '../Button/Button'
+import Router from 'next/router'
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
   const [search, setSearch] = useState<string>('')
 
+  const goToSearch = () => {
+    Router.push({
+      pathname: '/search',
+      query: {
+        q: search,
+      },
+    })
+  }
+
   return (
     <div className={cn(className, styles.search)} {...props}>
       <input placeholder="Search.." value={search} onChange={(e) => setSearch(e.target.value)} />
-      <Button appearance="primary" className={styles.button} onClick={() => {}}>
+      <Button appearance="primary" className={styles.button} onClick={goToSearch}>
         <GlassIcon />
       </Button>
     </div>
